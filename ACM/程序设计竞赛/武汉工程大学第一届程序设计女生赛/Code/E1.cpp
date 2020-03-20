1b20697f9f24bf21ccdcc69701ce35ad49076d56
unknown <you@example.com>
2020-03-20 22:14:25 +0800

    WIT ACM

--- ACM/程序设计竞赛/武汉工程大学第一届程序设计女生赛/Code/E1.cpp
+++ ACM/程序设计竞赛/武汉工程大学第一届程序设计女生赛/Code/E1.cpp
@@ -1,13 +1,11 @@
 # include <iostream>
 
-typedef long long ll;
+const int mod = 1000000007;
 
-const ll mod = 1000000007;
+int fphi(int n) { 
+	int result = n;
 
-ll fphi(ll n) { 
-	ll result = n;
-
-	for (ll p = 2; p * p <= n; ++p) { 		 
+	for (int p = 2; p * p <= n; p++) { 		 
 		if (n % p == 0) { 
 			while (n % p == 0) 
 				n /= p; 
@@ -22,63 +20,62 @@ ll fphi(ll n) {
 	return result; 
 } 
 
-ll qpow(ll x, ll p) {
-	ll ans = 1;
+int qpow(int x, int p) {
+	int ans = 1;
 	while (p) {
 		if (p & 1) {
-			ans = ans * x % mod;
+			ans = 1ll * ans * x % mod;
 		}
 		
-		x = x * x % mod;
+		x = 1ll * x * x % mod;
 		p >>= 1;
 	}
 	
 	return ans;
 }
 
-ll c(ll n, ll m) {
+int c(int n, int m) {
 	if (n < m) {
 		return 0;
 	}
 	
-	ll ntr = 1;
-	ll dom = 1;
+	int ntr = 1;
+	int dom = 1;
 	
 	for (int i=n-m+1; i<=n; i++) {
-		ntr = ntr * i % mod;
+		ntr = 1ll * ntr * i % mod;
 	}
 	
  	for (int i=1; i<=m; i++) {
- 		dom = dom * i % mod;	
+ 		dom = 1ll * dom * i % mod;	
 	}
 	
-	return ntr * qpow(dom, mod-2) % mod;
+	return 1ll * ntr * qpow(dom, mod-2) % mod;
 }
 
-ll lucas(ll n, ll m) {
+int lucas(int n, int m) {
 	if (0 == m) {
 		return 1;
 	} else {
-		return lucas(n / mod, m / mod) * c(n % mod, m % mod) % mod;
+		return 1ll * lucas(n / mod, m / mod) * c(n % mod, m % mod) % mod;
 	}
 }
 
 int main() {
 	int T;
 	std::cin >> T;
-	ll rev = qpow(2, fphi(mod - 1));
+	int rev = qpow(2, fphi(mod - 1));
 	
 	while (T--) {
-		ll n;
+		int n;
 		std::cin >> n;
 		
-		ll t1 = lucas(n, n/2);
+		int t1 = lucas(n, n/2);
 
-		ll t2 = qpow(rev, n);
+		int t2 = qpow(rev, n);
 		
-		std::cout << t1 * t2 % mod << "\n";
+		std::cout << 1ll * t1 * t2 % mod << "\n";
 	}
 	
 	return 0;
 }
-
